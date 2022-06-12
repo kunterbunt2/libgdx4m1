24f3ad26112b8662ffd77a90adb1b95d5c5ebbe7
kunterbunt <abdalla@bushnaq.de>
2022-06-12 17:00:52 +0200

    SpriteBatch + opengl 3.2 not working on macos with m1 chip

---
+++ desktop/src/com/mygdx/game/full/screen/SpriteBatch.java
@@ -0,0 +1,23 @@
+package com.mygdx.game.full.screen;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration.GLEmulation;
+import com.mygdx.game.Libgdx4M1;
+
+/*
+ * if stared will crash jre on mac mini with M1 ship.
+ * If you remove setFullscreenMode, it will work.
+ * 
+ */
+public class SpriteBatch {
+	public static void main(String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("libgdx4m1");
+
+		config.setOpenGLEmulation(GLEmulation.GL30, 3, 2);
+
+		new Lwjgl3Application(new Libgdx4M1(), config);
+	}
+}
